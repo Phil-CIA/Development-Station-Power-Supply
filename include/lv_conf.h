9739a9305cc18877e0df7746557e674656633cc1
Phil-CIA <forche@roadrunner.com>
2026-04-15 12:36:36 -0500

    Add verified LVGL demo target for ESP32 TFT bring-up

---
+++ include/lv_conf.h
@@ -0,0 +1,22 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+#define LV_COLOR_16_SWAP 0
+
+#define LV_MEM_CUSTOM 0
+#define LV_USE_LOG 0
+
+#define LV_HOR_RES_MAX 480
+#define LV_VER_RES_MAX 320
+
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_22 1
+
+#define LV_USE_LABEL 1
+#define LV_USE_BAR 1
+#define LV_USE_BTN 1
+#define LV_USE_ARC 1
+
+#endif
